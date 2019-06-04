9ea5b84bc712d352daf6d3ec6cc471e4f3c27de0
Ellen Buelow <buelowek1225@gmail.com>
2019-06-04 12:27:59 -0500

    First Heroku Push

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,15 @@
 -- Created the DB 
+DROP DATABASE IF EXISTS burger_db
+
 CREATE DATABASE burger_db;
 USE burger_db;
 
 -- Created the table "schools" 
 CREATE TABLE burgers
 (
-  id int AUTO_INCREMENT,
+  id int AUTO_INCREMENT NOT NULL,
   burger_name varchar (30) NOT NULL,
-  devoured BOOLEAN,
+  devoured BOOLEAN NOT NULL,
   PRIMARY KEY
   (id)
 );
